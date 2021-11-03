831fe2504cb50d1921ecf90db7ec6c4142835d4e
Eshaan Mathur <eshaan.mathur@gmail.com>
2021-11-03 12:04:56 +0530

    fix: on large size progress bar is rounden at 99%

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -8,21 +8,34 @@ import VisuallyHidden from '../VisuallyHidden';
 const sizes = {
     small: { height: 8, padding: 0, borderRadius: 4 },
     medium: { height: 12, padding: 0, borderRadius: 4 },
-    large: { height: 24, padding: 4, borderRadius: 8 },
+    large: { height: 16, padding: 4, borderRadius: 8 },
 };
 
 const ProgressBar = ({ value, size }) => {
+    const styles = sizes[size];
+
+    if (!styles) {
+        throw new Error(
+            `Unknown size value for the ProgressBar: ${size}. Valid options are ${Object.keys(sizes).join(' | ')}`,
+        );
+    }
+
     return (
         <>
             <Wrapper
-                {...sizes[size]}
+                style={{
+                    '--padding': styles.padding + 'px',
+                    '--radius': styles.borderRadius + 'px',
+                }}
                 role={'progressbar'}
                 aria-valuenow={value}
                 aria-valuemin={'0'}
                 aria-valuemax={'100'}
             >
                 <VisuallyHidden>{value}%</VisuallyHidden>
-                <Bar value={value} />
+                <BarWrapper>
+                    <Bar style={{ '--width': value + '%', '--height': styles.height + 'px' }} />
+                </BarWrapper>
             </Wrapper>
         </>
     );
@@ -31,16 +44,20 @@ const ProgressBar = ({ value, size }) => {
 const Wrapper = styled.div`
     position: relative;
     background-color: ${COLORS.transparentGray15};
-    overflow: hidden;
     box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-    border-radius: ${(props) => props.borderRadius || '4'}px;
-    height: ${(props) => props.height || 8}px;
-    padding: ${(props) => props.padding || 0}px;
+    border-radius: var(--radius, 4px);
+    padding: var(--padding, 0);
+`;
+
+const BarWrapper = styled.div`
+    overflow: hidden;
+    border-radius: 4px;
+    width: 100%;
 `;
 
 const Bar = styled.div`
-    height: 100%;
-    width: ${(props) => props.value ?? 100}%;
+    height: var(--height, 8px);
+    width: var(--width, 50%);
     background-color: ${COLORS.primary};
     border-radius: 4px 0 0 4px;
 `;
